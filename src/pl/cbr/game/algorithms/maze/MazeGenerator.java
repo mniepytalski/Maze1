d628ed4427e26e5f777212bee3d7e7362ae2d1e7
Mariusz Niepytalski <mariusz.niepytalski-extern@dbschenker.com>
2022-02-05 14:28:29 +0100

    Initial commit

---
+++ src/pl/cbr/game/algorithms/maze/MazeGenerator.java
@@ -0,0 +1,67 @@
+package pl.cbr.game.algorithms.maze;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+ * recursive backtracking algorithm
+ * shamelessly borrowed from the ruby at
+ * http://weblog.jamisbuck.org/2010/12/27/maze-generation-recursive-backtracking
+ */
+public class MazeGenerator {
+    private final int x;
+    private final int y;
+    private final int[][] maze;
+
+    public MazeGenerator(int x, int y) {
+        this.x = x;
+        this.y = y;
+        maze = new int[this.x][this.y];
+        generateMaze(0, 0);
+    }
+
+    public void display() {
+        for (int i = 0; i < y; i++) {
+            // draw the north edge
+            for (int j = 0; j < x; j++) {
+                System.out.print((maze[j][i] & 1) == 0 ? "+---" : "+   ");
+            }
+            System.out.println("+");
+            // draw the west edge
+            for (int j = 0; j < x; j++) {
+                System.out.print((maze[j][i] & 8) == 0 ? "|   " : "    ");
+            }
+            System.out.println("|");
+        }
+        // draw the bottom line
+        for (int j = 0; j < x; j++) {
+            System.out.print("+---");
+        }
+        System.out.println("+");
+    }
+
+    private void generateMaze(int cx, int cy) {
+        Direction[] dirs = Direction.values();
+        Collections.shuffle(Arrays.asList(dirs));
+        for (Direction dir : dirs) {
+            int nx = cx + dir.dx;
+            int ny = cy + dir.dy;
+            if (between(nx, x) && between(ny, y)
+                    && (maze[nx][ny] == 0)) {
+                maze[cx][cy] |= dir.bit;
+                maze[nx][ny] |= dir.opposite.bit;
+                generateMaze(nx, ny);
+            }
+        }
+    }
+
+    private static boolean between(int v, int upper) {
+        return (v >= 0) && (v < upper);
+    }
+
+    public static void main(String[] args) {
+        MazeGenerator maze = new MazeGenerator(42, 60);
+        maze.display();
+    }
+
+}
